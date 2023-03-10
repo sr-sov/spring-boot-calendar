0f75cf8242201a2dcd70b7e98d6e71f8ce9a4789
Joshua David Sta Rita <jdcstarita@gmail.com>
2023-03-10 09:08:44 +0800

    CORS default settings

--- spring-boot-app/src/main.jsx
+++ spring-boot-app/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-fetch('http://localhost:8080/api/content').then(response.json()).then(console.log(data))
+fetch('http://localhost:8080/api/content').then(response => response.json()).then(data => console.log(data))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/main/java/com/srsov/contentcalendar/controller/ContentController.java
+++ src/main/java/com/srsov/contentcalendar/controller/ContentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.srsov.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 	private final ContentCollectionRepository repository;
     
